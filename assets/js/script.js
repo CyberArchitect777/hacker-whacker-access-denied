f69ab7394b1554dd63ad6b554292fbb5c9f110e2
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-11-12 19:22:11 +0000

    chore: add GPL 3.0 license information to all relevant code files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
 
+/*
+Hacker Whacker: Access Denied: A hacker themed whack-a-mole style game
+Copyright (C) 2024 Barrie Millar
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // Minimum global variables implemented in place of using HTML hidden elements to hold values
 
 /**
